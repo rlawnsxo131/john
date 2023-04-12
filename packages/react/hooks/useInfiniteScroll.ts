72c761a398bd89f6067439634ea7004c3efa628d
juntae <rlawnsxo131@gmail.com>
2023-04-13 02:48:34 +0900

    refactor(react): 타입 수정

--- packages/react/hooks/useInfiniteScroll.ts
+++ packages/react/hooks/useInfiniteScroll.ts
@@ -11,7 +11,7 @@ import { usePreservedReference } from './usePreservedReference';
  */
 export function useInfiniteScroll<E extends HTMLElement = HTMLElement>(
   ref: RefObject<E>,
-  fetchNext?: () => void,
+  fetchNext: () => void,
   options: IntersectionObserverInit = {
     root: null,
     rootMargin: '0px',
@@ -26,7 +26,6 @@ export function useInfiniteScroll<E extends HTMLElement = HTMLElement>(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            if (!fetchNext) return;
             fetchNext();
           }
         });
