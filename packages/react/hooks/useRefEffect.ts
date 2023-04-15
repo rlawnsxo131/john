4cffcf1aa8ed63128c1a521af5904f0440eb4d73
juntae <rlawnsxo131@gmail.com>
2023-04-15 22:18:20 +0900

    comment(react): eslint 주의사항 추가

--- packages/react/hooks/useRefEffect.ts
+++ packages/react/hooks/useRefEffect.ts
@@ -15,6 +15,7 @@ export type EffectRefCallback<E extends HTMLElement = HTMLElement> = (
 /**
  * @link facebook/react#15176 - ref의 cleanup을 구현해달라고 요청하는 GitHub Issue
  * @description useRef에 저장되는 값에 안전하게 이펙트를 발생시키고자 할 때 사용할 수 있는 hook입니다.
+ * eslint react-hooks/exhaustive-deps 에 추가해서 안전하게 사용하세요.
  *
  * @param callback
  * @param deps
